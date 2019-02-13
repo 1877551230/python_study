62fc66c1a7d227449e8ead81a731a5294b65aa77
1877551230 <46470436+1877551230@users.noreply.github.com>
2019-02-13 22:13:18 +0800

    Delete download.py

--- download.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-
-respond = urllib.request.urlopen('http://placekitten.com/g/500/600')
-cat_img = respond.read()
-with open('cat_500_600.jpg','wb') as f:
-    f.write(cat_img)
